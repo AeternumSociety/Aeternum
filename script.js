81e9dab33fcbc8f4ae830cf43c7c81f9cafd485f
AeternumSociety <142216363+AeternumSociety@users.noreply.github.com>
2023-08-14 12:00:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const symbol = document.querySelector('img');
+    symbol.addEventListener('click', function() {
+        revealSecret();
+    });
+});
+
+function revealSecret() {
+    // Aquí puedes mostrar un mensaje o desencadenar la siguiente parte del juego.
+}
